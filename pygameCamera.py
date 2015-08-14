e8acecdf6d5d4430df9fa58100815b7bcc1123ab
Benjamin Secker <benjamin.secker@gmail.com>
2015-08-15 08:30:11 +1200

    Final(ish) release
    Bug Fixes and working with calibration

--- pygameCamera.py
+++ pygameCamera.py
@@ -1,5 +1,5 @@
 #Arduino PyGame Camera and Serial Communication Module.
-
+ 
 import sys
 import pygame
 import pygame.camera
@@ -7,14 +7,14 @@ from pygame.locals import *
 import colorsys
 import time
 import serial
-
+ 
 class ArduinoSerial:
     def __init__(self):
         self.ser = serial.Serial('/dev/ttyUSB3',9600)
         collect_readings = False
         self.colours = []
         self.max_readings = 50 #maximum number of readings to use
-
+ 
     def add(self, colour):
         self.colours.append(colour)
         if len(self.colours) >= self.max_readings:
@@ -23,8 +23,8 @@ class ArduinoSerial:
             self.writeOut(_mode)
             collect_readings = False
             self.colours = []
-
-        
+ 
+         
     def writeOut(self, colour):
         """Send commands to arduino"""
         _col = ""
@@ -39,7 +39,7 @@ class ArduinoSerial:
         elif colour == "cyan":
             _col = "4"
         self.ser.write(_col)
-
+ 
 def calibrate():
     """startup function to calibrate the colour sensing"""
     print 'Performing initial calibration sequence...'
@@ -56,11 +56,11 @@ def calibrate():
         crect = pygame.draw.rect(screen, (255,255,255), (145, 105, 50,50), 3)
         ccolor = pygame.transform.average_color(avg,crect)
         hsv = colorsys.rgb_to_hsv(ccolor[0]/255.0, ccolor[1]/255.0, ccolor[2]/255.0 )
-        print hsv[0]    
-
+        print hsv[0]   
+ 
         screen.fill(ccolor, (0,0,50,50))
         pygame.display.flip()
-        
+         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 webcam.stop()
@@ -70,38 +70,38 @@ def calibrate():
                 if event.key == K_1:
                     _hues[0][0] = raw_input('REDMIN:')
                     _hues[0][1] = raw_input('REDMAX:')
-
+ 
                 #orange
                 if event.key == K_2:
                     _hues[1][0] = raw_input('ORANGEMIN:')
                     _hues[1][1] = raw_input('ORANGEMAX:')
-
+ 
                 #yellow
                 if event.key == K_3:
                     _hues[2][0] = raw_input('YELLOWMIN:')
                     _hues[2][1] = raw_input('YELLOWMAX:')
-
+ 
                 #cyan
                 if event.key == K_4:
                     _hues[3][0] = raw_input('CYANMIN:')
                     _hues[3][1] = raw_input('CYANMAX:')
-
+ 
                 #done
                 if event.key == K_d:
                     print 'done calibration'
-
-
+ 
+ 
     print 'original hues: [[31,331],[31,43],[43,57],[151,211]'
     print 'calibrated hues:',_hues
     return _hues
     #return [[31,331],[31,43],[43,57],[151,211]
-
+ 
 def get_colour(hsv):
     """takes HSV input and returns one of the possible colours"""
     if hsv[1]<0.2 : # check saturation. Can't use value because of AGC
         detected = 'none'
-
-        #00 - none, 01 - red, 10 - green 11 - blue 
+ 
+        #00 - none, 01 - red, 10 - green 11 - blue
     else:
         # print int(hsv[0]*360), int(hsv[1]*100), int(hsv[2]*100),ccolor
         # conver HSV to color words
@@ -118,81 +118,75 @@ def get_colour(hsv):
         else:
             detected = 'none'
     return detected
-
+ 
 def terminate():
     """flush serial buffer and close everything down nicely"""
     arduino_serial.ser.close()
     pygame.quit()
     sys.exit()
-    
-
+     
+ 
 def main():
     pygame.camera.init()
-    
+     
     global arduino_serial, collect_readings, hue_list
     collect_readings = False
     arduino_serial = ArduinoSerial()
-
+ 
     hue_list = calibrate()
-        
+         
     screen = pygame.display.set_mode((320,240),0)
     cam_list = pygame.camera.list_cameras()
-    
+     
     if not cam_list:
         print 'Error: No camera found. quitting'
         terminate()
-        
+         
     webcam = pygame.camera.Camera(cam_list[0],(352,288))
-    
+     
     try:
         webcam.start()
     except SystemError:
         pygame.quit()
         sys.exit()
-
+ 
     detected = 'none'
     last_detected = ''
-
+ 
     #calibrate colours
     calibrate()
-
+ 
     while True:
         # reduce noise, average 5 frames
         av = []
         for i in range(0,1):
             av.append(webcam.get_image())
-            
+             
         avg = pygame.transform.average_surfaces(av)
         screen.blit(avg, (0,0))     # display average
         # find the average colour inside the target rectangle
         crect = pygame.draw.rect(screen, (255,255,255), (145, 105, 50,50), 3)
         ccolor = pygame.transform.average_color(avg,crect)
         hsv = colorsys.rgb_to_hsv(ccolor[0]/255.0, ccolor[1]/255.0, ccolor[2]/255.0 )
-        
+         
         detected = get_colour(hsv)
-        
-              
+         
+               
         if collect_readings == True:
             print detected
             arduino_serial.add(detected)
-            
-
+             
+ 
         screen.fill(ccolor, (0,0,50,50))
         pygame.display.flip()
-        
+         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 webcam.stop()
                 terminate()
             elif event.type
-            
-
+             
+ 
 if __name__=="__main__":
     main()
-
-
-
-                              
-
-
-                              
+ 
